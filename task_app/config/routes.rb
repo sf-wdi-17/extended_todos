841dc8ed23ccec18137bc5a50a7aa75f24fa8e4d
Dennis Charles Hackethal <dennis@generalassemb.ly>
2015-04-29 11:21:47 -0700

    Implement authentication

--- task_app/config/routes.rb
+++ task_app/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :authors
   root to: 'authors#index'
 
   get '/authors', to: 'authors#index'
   get '/authors/:id', to: 'authors#show'
   get '/authors/:id/todos', to: 'todos#index'
 
-  post '/authors', to: 'authors#create'
   post '/authors/:id/todos', to: 'todos#create'
 end
